9f13588961acd8943c338c6708c4099d736d23b7
shravyakulal01 <kulalshravya72@gmail.com>
2025-05-22 16:39:54 +0530

    My updated assignment files

--- Lab8JSPandJDBC-1/UsingJspForQuery8C-8D/database.sql
+++ Lab8JSPandJDBC-1/UsingJspForQuery8C-8D/database.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE IF NOT EXISTS Employee;
-
 USE Employee;
-
 CREATE TABLE IF NOT EXISTS Emp (
     Emp_NO INT PRIMARY KEY,
     Emp_Name VARCHAR(100) NOT NULL,
     Basicsalary INT
 );
+INSERT INTO Emp (Emp_NO, Emp_Name, Basicsalary) VALUES
+(102, 'Ravi', 10000),
+(104, 'seetha', 200000),
+(105, 'Rajesh', 30000),
+(256, 'shravya', 500000);
+(344, 'ram', 76555),
