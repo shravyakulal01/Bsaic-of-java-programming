5f5c84e8bf9a55cab74f1d7d80913f807ee9355a
shravyakulal01 <kulalshravya72@gmail.com>
2025-05-22 16:28:58 +0530

    My updated assignment files

--- Lab8JSPandJDBC-1/ReadingQuery8A-8B/database.sql
+++ Lab8JSPandJDBC-1/ReadingQuery8A-8B/database.sql
@@ -5,3 +5,6 @@ AUTO_INCREMENT,`coffee_name` varchar(100) NOT NULL,`priced` int(5), PRIMARY KEY 
 INSERT INTO coffee values (101,'Febary',120);
 INSERT INTO coffee values (102,'Royal',140);
 INSERT INTO coffee values (103,'Diamond',180);
+INSERT INTO coffee values (104,'Duridpl',280);
+INSERT INTO coffee values (105,'Dasimy',200);
+INSERT INTO coffee values (108,'Dhamdh',300);
